01cb06756c6059dbd5f017ea44d98e1c416eae20
pilski <piotr.ilski@gft.com>
2016-09-06 15:35:27 +0200

    Support CORS

--- src/app.js
+++ src/app.js
@@ -20,8 +20,14 @@
 
     let express = require('express');
     let _ = require('lodash');
-    let app = express();
-    
+    let app = express();    
+
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.get('/', (req, res) => {
         res.json(aliveJson);
     });
